dfa155d080f5cdeafacc447227fec445fa929753
Scott Stubblefield <sstubblefield@users.noreply.github.com>
2022-10-14 10:05:47 -0400

    Update HAVTools.user.js

--- HAVTools.user.js
+++ HAVTools.user.js
@@ -1,4 +1,5 @@
 document.getElementsByTagName("head")[0].innerHTML += '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+document.getElementsByTagName("head")[0].innerHTML += '<link href="https://sstubblefield.github.io/HAVTools.user.css" rel="stylesheet" type="text/css" />';
 
 var path = window.location.pathname;
 
