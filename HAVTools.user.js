ae3f81d981fe3db69351981da9a40e89afd06e87
Scott Stubblefield <sstubblefield@users.noreply.github.com>
2022-10-14 09:59:19 -0400

    Update HAVTools.user.js

--- HAVTools.user.js
+++ HAVTools.user.js
@@ -1,1 +1,8 @@
 document.getElementsByTagName("head")[0].innerHTML += '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+
+var path = window.location.pathname;
+
+path = path.split('/');
+pageName = path[path.length - 1];
+
+document.body.classList.add(pageName);
